6737e00c831a7055b5fc825d2eb1f25b4073a0dc
@ForSerious <forxxserious@gmail.com>
2017-08-25 08:08:40 -0400

    Renamed AlbumDAO, Made some progress on it's implementation.

--- venderbender/src/main/java/com/revature/data/AlbumDAO.java
+++ venderbender/src/main/java/com/revature/data/AlbumDAO.java
@@ -3,7 +3,7 @@
  */
 package com.revature.data;
 
-import java.util.Set;
+import java.util.List;
 
 import com.revature.beans.Album;
 
@@ -11,7 +11,7 @@ import com.revature.beans.Album;
  * @author Russ Barnes
  * Simple data access object for the artist bean.
  */
-public interface ArtistDAO {
+public interface AlbumDAO {
 	/**
 	 * Returns a single album from a known identification number.
 	 * @param idNumber the id number in the database.
@@ -23,23 +23,23 @@ public interface ArtistDAO {
 	 * @param artist, the string version of the artist.
 	 * @return a set of all the albums found.
 	 */
-	public Set<Album> getAlbumsByArtist(String artist);
+	public List<Album> getAlbumsByArtist(String artist);
 	/**
 	 * Mainly to find all albums of type CD or LP—most fanatics want one but not the other.
 	 * @param cd true for CD false for Vinyl  
 	 * @return a set of all the albums found.
 	 */
-	public Set<Album> getAlbumsByType(boolean cd);
+	public List<Album> getAlbumsByType(boolean cd);
 	/**
 	 * gets every album in the database. We try to split this up once we learn how.
 	 * @return a set of all albums.
 	 */
-	public Set<Album> getAllAlbums();
+	public List<Album> getAllAlbums();
 	/**
 	 * Who wants to even bother looking at jazz or country albums. Forget that and use this.
 	 * @param genre, the string version of the genre.
 	 * @return a set of all the albums found.
 	 */
-	public Set<Album> getAlbumsByGenre(String genre);
+	public List<Album> getAlbumsByGenre(String genre);
 
 }
---
+++ venderbender/src/main/java/com/revature/data/AlbumDAOImpl.java
@@ -0,0 +1,59 @@
+package com.revature.data;
+
+import java.util.List;
+import java.util.Set;
+
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+
+import com.revature.beans.Album;
+
+public class AlbumDAOImpl implements AlbumDAO {
+	private SessionFactory sessionFactory;
+
+	public AlbumDAOImpl() {
+		super();
+	}
+
+	public AlbumDAOImpl(SessionFactory sessionFactory) {
+		super();
+		this.sessionFactory = sessionFactory;
+	}
+
+	@Override
+	public Album getAlbumById(int idNumber) {
+		int id = idNumber;
+		if(id < 1){
+			//We may want to throw an exception here.
+			return null;
+		}
+		return (Album) sessionFactory.getCurrentSession().get(Album.class, id);
+	}
+
+	@Override
+	public List<Album> getAlbumsByArtist(String artist) {
+		Criteria query = sessionFactory.getCurrentSession().createCriteria(Album.class);
+		query.add(Restrictions.ilike("artist", artist));
+		return query.list();
+	}
+
+	@Override
+	public List<Album> getAlbumsByType(boolean cd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Album> getAllAlbums() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Album> getAlbumsByGenre(String genre) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
