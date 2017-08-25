b23b8579049f1d5b09b7b8668accbaf41a298260
Patrick <pburns96@live.com>
2017-08-25 14:37:47 -0400

    Added OrderDAOTest

--- venderbender/src/test/java/com/revature/test/OrderDAOTest.java
+++ venderbender/src/test/java/com/revature/test/OrderDAOTest.java
@@ -3,30 +3,30 @@ package com.revature.test;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 
+import com.revature.data.OrderDAO;
+
 public class OrderDAOTest {
 
 	ApplicationContext context;
-	
+
 	@Test
-	public void createOrderAndItemTest()
-	{
-		OrderDAO dao = context.getBean("OrderDAO");
+	public void createOrderAndItemTest() {
+		OrderDAO dao = (OrderDAO) context.getBean("OrderDAO");
 	}
-	
+
 	@Test
-	public void getOrderAndItemTest()
-	{
-		
+	public void getOrderAndItemTest() {
+
 	}
+
 	@Test
-	public void updateOrderItemTest()
-	{
-		
+	public void updateOrderItemTest() {
+
 	}
+
 	@Test
-	public void deleteOrdersAndItemTest()
-	{
-		
+	public void deleteOrdersAndItemTest() {
+
 	}
-	
+
 }
