ed7abd0dc2f5874d08210449ffe319dee2283775
Patrick <pburns96@live.com>
2017-08-29 20:01:42 -0400

    Added cart functionality and tried defining a list to store order client side

--- venderbender/src/main/webapp/static/controllers/cart.js
+++ venderbender/src/main/webapp/static/controllers/cart.js
@@ -1,4 +1,8 @@
-angular.module("VenderBender").controller("cartController", function($http, $scope) {
+angular.module("VenderBender").controller("cartController", function($http, $scope,$rootScope) {
 	
-	
+	$buyOrder = $http.post("/createOrder", cart.order).then(function(resp) {
+		
+		//send user back to the shop
+		//empty car order
+	})
 })
--- venderbender/src/main/webapp/static/controllers/home.js
+++ venderbender/src/main/webapp/static/controllers/home.js
@@ -1,4 +1,43 @@
-
-angular.module("VenderBender").controller("homeController",function(){
+//TODO ASK PATRICK
+angular.module("VenderBender").controller("homeController",function($scope,$http,$rootScope){
+	var items = []
+	$rootScope.cart = {
+	        total : 0,
+	        orders : {
+	        	items : {
+	        		item: {
+	        			id:0,
+	        			album : null,
+	        			concert:null,
+	        			quantity:1
+	        		}
+	        	}
+	        }
+	    };
 	
+	//addItem method(item) and check for similarity
+	//Adds item to cart.order.items
+	$rootScope.addItem = function(item){
+		
+		//Check and see if the item already exist
+		var exist = false;
+		angular.forEach($rootScope.car.order.items, function(listItem, index) {
+				//If the item already exist then add to the existing quantity
+			  if(listItem.id=item.id)
+			  	{
+				  exist = true;
+				  $rootScope.car.order.items[index].quantity += item.quantity;
+				}
+			});
+		
+		if(exist ==false)
+			{
+		$rootScope.car.order.items.push(item);
+			}
+	}
+	//add all functions for adding orders to cart
+	//cart should be tracked throughout switching of controllers
+	
+	
+	//
 })
